ab840295c1485076ef4047646b8ecd4a92466c00
Greg Wilson <greg.wilson@centralsquare.com>
2019-08-01 11:26:06 -0400

    lesson 2 fun

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,7 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string StartCommand = "start";
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -19,23 +20,102 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            //Console.WriteLine($"this is what I read: {message}");
-            if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Terminate();
-                return;
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
             }
 
+            GetAndValidateInput();
 
-            // send input to the console writer to process and print
-            _consoleWriterActor.Tell(read);
+            //if (message is Messages.InputError)
+            //{
+            //    var msg = message as Messages.InputError;
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine(msg.Reason);
+            //}
+            //else
+            //{
+            //    Unhandled(message);
+            //}
 
-            // continue reading messages from the console
-            Self.Tell("continue");
+            //var read = Console.ReadLine();
+            ////Console.WriteLine($"this is what I read: {message}");
+            //if (!string.IsNullOrEmpty(read) && String.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            //{
+            //    // shut down the system (acquire handle to system via
+            //    // this actors context)
+            //    Context.System.Terminate();
+            //    return;
+            //}
+
+
+            //// send input to the console writer to process and print
+            //_consoleWriterActor.Tell(read);
+
+            //// continue reading messages from the console
+            //Self.Tell("continue");
         }
 
+        #region Internal Methods
+
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want to the console!");
+            Console.WriteLine("Some entries will pass validation, and some will NOT...\n\n");
+            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        }
+
+        /// <summary>
+        /// reads input from console, validates it, then signals appropriate response
+        /// (continu processing, error, success, etc.).
+        /// </summary>
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+            if (string.IsNullOrEmpty(message))
+            {
+                //signal that the user needs to supply an input, as previously
+                //received input was blank
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else if (String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                //shut down the entire actor system (allows process to exit)
+                Context.System.Terminate();
+            }
+            else
+            {
+                var valid = IsValid(message);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+
+                    // continue reading messages from console
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Validates <see cref="message"/>.
+        /// Currently says messages are valid if contain even number of characters.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+ 
+        private static bool IsValid(string message)
+        {
+            var valid = message.Length % 2 == 0;
+            return valid;
+        }
+        #endregion
     }
 }
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
 ﻿using Akka.Actor;
+using System;
 
 namespace WinTail
 {
@@ -12,9 +12,9 @@ namespace WinTail
         {
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
-            Console.WriteLine("MyActorSystem has been created");
+            Console.WriteLine("\n\n MyActorSystem has been created\n\n");
 
-            PrintInstructions();
+            //PrintInstructions();
 
             // time to make your first actors!
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
@@ -25,27 +25,27 @@ namespace WinTail
 
             // tell console reader to begin
             Console.WriteLine("telling the readerActor to begin...........");
-            consoleReaderActor.Tell("Say something I'm giving up on you");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
         }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.Write(" magenta ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Write(" NOT green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
+        //private static void PrintInstructions()
+        //{
+        //    Console.WriteLine("Write whatever you want into the console!");
+        //    Console.Write("Some lines will appear as");
+        //    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        //    Console.Write(" magenta ");
+        //    Console.ResetColor();
+        //    Console.Write(" and others will appear as");
+        //    Console.ForegroundColor = ConsoleColor.Blue;
+        //    Console.Write(" NOT green! ");
+        //    Console.ResetColor();
+        //    Console.WriteLine();
+        //    Console.WriteLine();
+        //    Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+        //}
     }
     #endregion
 }
